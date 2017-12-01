32d521cbb4bc56c32ab91cbe516183c851ae88b0
cytonn <mjmaina@cytonn.com>
2017-12-01 13:22:22 +0300

    Initial commit

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\Thread;
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+	    'user_id' => function (){
+		    return factory('App\User')->create()->id;
+	    },
+	    'title' => $faker->sentence,
+	    'body' => $faker->paragraph
+    ];
+});
